3c719ac399ecddef21b48d2757f2197c83a585f3
sabira <sabira@sabira-PC>
2020-09-11 13:16:00 +0530

    new test data

---
+++ APIAutomation/src/test/java/com/api/testScripts/GetDataFromPosts.java
@@ -0,0 +1,19 @@
+package com.api.testScripts;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.api.genericLib.BaseClass;
+
+import io.restassured.response.Response;
+
+public class GetDataFromPosts extends BaseClass {
+    @Test
+	public void getData() throws IOException
+	{
+		String apiTestData = config.getApiTestData("getAllPosts");
+		Response resp = config.executeRequest(apiTestData);
+		resp.then().log().all();
+	}
+}
